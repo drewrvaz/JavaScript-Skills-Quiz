e0a0887aa531547e8477ec02b51dcc8184fcc946
drewrvaz <drewrvaz@gmail.com>
2022-08-11 10:36:27 -0400

    recoded the show question function, running into an issue with id showing up as undefined

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,15 @@
 var timeRemaining = document.getElementById("time-remaining");
 var startButton = document.getElementById("start-button");
-var quizInstructions = document.getElementById("instruction-card")
-var quizQuestions = document.getElementById("question-card")
-var quizScore = document.getElementById("score-card")
-var quizHighScores = document.getElementById("highscore-card")
-var questionEl = document.getElementById("question")
-var answerButtons = document.getElementById("answer")
+var quizInstructions = document.getElementById("instruction-card");
+var quizQuestions = document.getElementById("question-card");
+var quizScore = document.getElementById("score-card");
+var quizHighScores = document.getElementById("highscore-card");
+var questionEl = document.getElementById("question");
 
 //An array to store all the questions and their answers
 var questionIndex = [
   {
+    id: 0,
     question: "Commonly used data types DO NOT include:",
     answers: [ 
       {text: "1) strings",  isCorrect: false},
@@ -18,7 +18,7 @@ var questionIndex = [
       {text: "4) numbers", isCorrect: false}]
   },
   {
-    id: 0,
+    id: 1,
     question: "The condition in an if/else statement is enclosed within ______.",
     answers: [ 
       {text: "1) quotes",  isCorrect: false},
@@ -27,7 +27,7 @@ var questionIndex = [
       {text: "4) square brackets", isCorrect: false}]
   },
   {
-    id: 1,
+    id: 2,
     question: "Arrays in JavaScript can be used to store ______.",
     answers: [ 
       {text: "1) numbers and strings",  isCorrect: false},
@@ -36,7 +36,7 @@ var questionIndex = [
       {text: "4) all of the above", isCorrect: true}]
   },
   {
-    id: 2,
+    id: 3,
     question: "String values must be enclosed within ______ when being assigned in variables.",
     answers: [ 
       {text: "1) commas",  isCorrect: false},
@@ -45,7 +45,7 @@ var questionIndex = [
       {text: "4) parentheses", isCorrect: false}]
   },
   {
-    id: 3,
+    id: 4,
     question: "A very useful tool during development and debugging for printing content to the debugger is:",
     answers: [
       {text: "1) JavaScript",  isCorrect: false},
@@ -53,18 +53,17 @@ var questionIndex = [
       {text: "3) for loops", isCorrect: false}, 
       {text: "4) console.log", isCorrect: true}]
   }
-]
+];
 
 // Event listener on the start button to begin the quiz
-startButton.addEventListener("click", startQuiz) 
+startButton.addEventListener("click", startQuiz); 
 
 function startQuiz() {
-  countdown()
-  quizInstructions.classList.add("hide-card")
-  quizQuestions.classList.remove("hide-card")
-  quizQuestions.classList.add("card")
-  questionIndex = 0  
-  showQuestion()
+  countdown();
+  quizInstructions.classList.add("hide-card");
+  quizQuestions.classList.remove("hide-card");
+  quizQuestions.classList.add("card");
+  showQuestion();
 }
 
 // Function for the countdown clock
@@ -82,9 +81,26 @@ function countdown() {
   }, 1000);
 }
 
-function showQuestion(questionEl) {
-  questionEl.innerText = questionEl.question
+function showQuestion() {
+  // Sets question text
+  questionEl.innerText = questionIndex[id].question;
+  // Calls the answer options
+  var answer1 = document.getElementById("answer-1");
+  var answer2 = document.getElementById("answer-2");
+  var answer3 = document.getElementById("answer-3");
+  var answer4 = document.getElementById("answer-4");
+  // Sets the answer text
+  answer1.innerText = questionIndex[id].answers[0].text;
+  answer2.innerText = questionIndex[id].answers[1].text;
+  answer3.innerText = questionIndex[id].answers[2].text;
+  answer4.innerText = questionIndex[id].answers[3].text;
+  // Sets the true or false value for each answer
+  answer1.value = questionIndex[id].answers[0].isCorrect;
+  answer2.value = questionIndex[id].answers[1].isCorrect;
+  answer3.value = questionIndex[id].answers[2].isCorrect;
+  answer4.value = questionIndex[id].answers[3].isCorrect;
 }
+console.log(showQuestion)
 
 function endQuiz() {
 quizQuestions.classList.add("hide-card")
