56b63c90e9f82a38714542817142128d66c02e22
drewrvaz <drewrvaz@gmail.com>
2022-08-11 00:38:20 -0400

    Added vh and vw to the cards. Began writing the function to start the quiz

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,24 @@
-var timeRemaining = document.getElementById("#time-remaining");
-var startButton = document.getElementById("#start-button");
+var timeRemaining = document.getElementById("time-remaining");
+var startButton = document.getElementById("start-button");
 
 // Event listener on the start button to begin the quiz
-startButton.addEventListener("click", startQuiz);
+startButton.addEventListener("click", startQuiz) 
 
 function startQuiz() {
-  function countdown() {
-    var timeLeft = 75;
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 0) {
-        timeLeft;
-      } else {
-        timeRemaining.textContent = "";
-        clearInterval(timeInterval);
-        endQuiz;
-      }
-    })
-  }
+  countdown
+}
+
+function countdown() {
+  var timeLeft = 75;
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timeRemaining.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      timeRemaining.textContent = "";
+      clearInterval(timeInterval);
+      endQuiz;
+    }
+  }, 1000);
 }
 
