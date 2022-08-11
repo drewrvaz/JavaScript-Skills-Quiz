8e4a2a56256fc9eb157d3b5e108fd03a2ecd0200
drewrvaz <drewrvaz@gmail.com>
2022-08-11 13:10:53 -0400

    Added cards for each of the questions. Also removed the way I had been cycling through the questions earlier since it wasn't working

--- Assets/script.js
+++ Assets/script.js
@@ -6,55 +6,6 @@ var quizScore = document.getElementById("score-card");
 var quizHighScores = document.getElementById("highscore-card");
 var questionEl = document.getElementById("question");
 
-//An array to store all the questions and their answers
-var questionIndex = [
-  {
-    id: 0,
-    question: "Commonly used data types DO NOT include:",
-    answers: [ 
-      {text: "1) strings",  isCorrect: false},
-      {text: "2) booleans", isCorrect: false}, 
-      {text: "3) alerts", isCorrect: true}, 
-      {text: "4) numbers", isCorrect: false}]
-  },
-  {
-    id: 1,
-    question: "The condition in an if/else statement is enclosed within ______.",
-    answers: [ 
-      {text: "1) quotes",  isCorrect: false},
-      {text: "2) curly brackets", isCorrect: false}, 
-      {text: "3) parentheses", isCorrect: true}, 
-      {text: "4) square brackets", isCorrect: false}]
-  },
-  {
-    id: 2,
-    question: "Arrays in JavaScript can be used to store ______.",
-    answers: [ 
-      {text: "1) numbers and strings",  isCorrect: false},
-      {text: "2) other arrays", isCorrect: false}, 
-      {text: "3) booleans", isCorrect: false}, 
-      {text: "4) all of the above", isCorrect: true}]
-  },
-  {
-    id: 3,
-    question: "String values must be enclosed within ______ when being assigned in variables.",
-    answers: [ 
-      {text: "1) commas",  isCorrect: false},
-      {text: "2) curly brackets", isCorrect: false}, 
-      {text: "3) quotes", isCorrect: true}, 
-      {text: "4) parentheses", isCorrect: false}]
-  },
-  {
-    id: 4,
-    question: "A very useful tool during development and debugging for printing content to the debugger is:",
-    answers: [
-      {text: "1) JavaScript",  isCorrect: false},
-      {text: "2) terminal/bash", isCorrect: false}, 
-      {text: "3) for loops", isCorrect: false}, 
-      {text: "4) console.log", isCorrect: true}]
-  }
-];
-
 // Event listener on the start button to begin the quiz
 startButton.addEventListener("click", startQuiz); 
 
